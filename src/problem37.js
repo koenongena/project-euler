f191daaf89df86e4538d656d656bebd9180f3502
koen.ongena <gijoke@gmail.com>
2021-07-31 21:03:00 +0200

    Problem 37 - refactored

--- src/problem37.js
+++ src/problem37.js
@@ -8,16 +8,22 @@ const numberTail = n => parseInt(R.tail(n.toString()), 10);
 const isPrime = n => n > 1 && isBasePrime(n);
 
 const isLeftTruncatablePrime = (n) => {
-    return (n < 10 && isPrime(n)) || (isPrime(n) && isLeftTruncatablePrime(numberTail(n)));
-}
-const isRightTruncatablePrime = (n) => {
-    return (n < 10 && isPrime(n)) || (isPrime(n) && isRightTruncatablePrime(numberInit(n)));
-}
+    if (n < 10) {
+        return isPrime(n)
+    }
 
-const isTruncatablePrime = (n) => {
-    return isPrime(n) && isLeftTruncatablePrime(n) && isRightTruncatablePrime(n);
+    return isPrime(n) && isLeftTruncatablePrime(numberTail(n));
 };
+const isRightTruncatablePrime = (n) => {
+    if (n < 10) {
+        return isPrime(n)
+    }
+    return isPrime(n) && isRightTruncatablePrime(numberInit(n));
+};
+
+const isTruncatablePrime = R.allPass([isLeftTruncatablePrime, isRightTruncatablePrime]);
+
 (() => {
-    const result = R.range(10, 10_000_000).filter(isTruncatablePrime);
+    const result = R.range(10, 1_000_000).filter(isTruncatablePrime);
     console.log(R.sum(result));
 })();
